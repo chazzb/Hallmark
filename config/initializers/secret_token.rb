d5175cad2929f98bbd239b524e7a4c28ce12ec07
Charles Bell <charlesbell@charless-mbp.home>
2014-01-02 22:21:40 +0000

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Hallmark::Application.config.secret_key_base = '48fce878a058a8603b6fa14a18001397fef289a0f7bdc3a3b82821bf4f4523f886be17a81c0408b121397e5298be0fcebdac1e357447d50f333bd57b2d540aef'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Hallmark::Application.config.session_store :cookie_store, key: '_Hallmark_session'
